da7842780549ad5afcd60b71590a5f7764c3f555
gauenk <kent.gauen@gmail.com>
2022-08-02 22:43:49 -0400

    init pacnet commit

---
+++ lib/setup.py
@@ -0,0 +1,31 @@
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
+from __future__ import print_function
+from setuptools import setup, find_packages
+# from distutils.core import setup
+import os
+import stat
+import shutil
+import platform
+import sys
+import site
+import glob
+
+
+# -- file paths --
+long_description="""PaCNet: Python implementation of PaCNet"""
+setup(
+    name='colanet',
+    version='100.100.100',
+    description='A python implementation of PaCNet',
+    long_description=long_description,
+    url='https://github.com/gauenk/cola_net',
+    author='Kent Gauen',
+    author_email='gauenk@purdue.edu',
+    license='MIT',
+    keywords='burst denoising, non-local search, video denoising, neural network',
+    install_requires=['numpy','torch','flake8'],
+    packages=find_packages(),
+)
